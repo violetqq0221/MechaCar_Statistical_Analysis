2103b7a6c1bb301520b206310742b4203b28aa53
Violet Huang <Violet.Huang@headway.com>
2023-01-14 18:21:05 -0800

    add all files

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,46 @@
+# part 1
+library(dplyr)
+mechacar_df <- read.csv(file="MechaCar_mpg.csv",check.names=F,stringsAsFactors=F)
+head(mechacar_df)
+model = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance
+           + AWD, data=mechacar_df)
+summary(model)
+
+# p-value: 5.35e-11
+## Linear Regression to Predict MPG ##
+# 1. vehicle_length , vehicle_weight, and ground_clearance variables provides a non-random
+#    amount of variance to the mpg variable.  We can see that because their Pr values are
+#    very small.
+# 2. p-value smaller than 0.05%, therefore sufficient evidence to reject null-hypothesis,
+#    which means slop of our linear model is not zero.
+# 3. v
+
+# part 2
+suspension_df <- read.csv(file="Suspension_Coil.csv",check.names=F,stringsAsFactors=F)
+#View(suspension_df)
+total_summary = summarize(suspension_df, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),
+                          SD=sd(PSI))
+View(total_summary)
+lot_summary = suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), 
+                                                                          Median=median(PSI), 
+                                                                          Variance=var(PSI),
+                                                                          SD=sd(PSI))
+View(lot_summary)
+# The current manuafacturing data shows that the design specification is met in all lots
+# in total because the total variance is only 62.  It is not met in lot 3 because breaking
+# down by lots shows lot3 has high variance of 170.
+
+# part 3
+t.test(log10(suspension_df$PSI),mu=mean(log10(suspension_df$PSI)))
+# against lot1
+lot1 <- subset(suspension_df, Manufacturing_Lot == "Lot1")
+#View(lot1)
+t.test(log10(lot1$PSI),mu=mean(log10(suspension_df$PSI)))
+# against lot2
+lot2 <- subset(suspension_df, Manufacturing_Lot == "Lot2")
+#View(lot2)
+t.test(log10(lot2$PSI),mu=mean(log10(suspension_df$PSI)))
+# against lot3
+lot3 <- subset(suspension_df, Manufacturing_Lot == "Lot3")
+#View(lot3)
+t.test(log10(lot3$PSI),mu=mean(log10(suspension_df$PSI)))
